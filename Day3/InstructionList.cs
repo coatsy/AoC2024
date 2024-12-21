6f745e6313ea619b36b95989a8175b66b8e3158c
Andrew Coates <coatsy@outlook.com>
2024-12-21 15:46:41 +1100

    Day 3.1

---
+++ Day3/InstructionList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    internal class InstructionList
+    {
+        private List<Instruction> instructions = new List<Instruction>();
+        public InstructionList(string input) {
+            string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+            var matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                var instruction = new Instruction();
+                var values = match.Value.Replace("mul(", "").Replace(")", "").Split(',');
+                instruction.m1 = int.Parse(values[0]);
+                instruction.m2 = int.Parse(values[1]);
+                instructions.Add(instruction);
+            }
+        }
+
+        public int ProductSum => instructions.Sum(i => i.Product);
+    }
+
+    internal class Instruction
+    {
+        public int m1, m2;
+        public int Product => m1 * m2;
+
+
+    }
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("./input/input.txt");
+            var instructionList = new InstructionList(input);
+
+            Console.WriteLine($"Sum of products: {instructionList.ProductSum}");
+        }
+    }
+}
