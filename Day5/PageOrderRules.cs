76f76d10c46f6e260316181b8f1fc7bd9dcc8b9b
Andrew Coates <coatsy@outlook.com>
2024-12-25 00:31:51 +1100

    Day 5.2 tests work, but real input fails

---
+++ Day5/PageOrderRules.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class PageOrderRules
+    {
+        private readonly List<Tuple<int, int>> rules = [];
+        private readonly List<int> pageOrder = [];
+        private bool orderCurrent = false;
+
+        public void AddRule(int lower, int higher)
+        {
+            rules.Add(Tuple.Create(lower, higher));
+            orderCurrent = false;
+        }
+        public void CalculateOrder()
+        {
+            if (orderCurrent) return;
+
+            var set = rules.Where(r => !pageOrder.Contains(r.Item1)).ToList();
+            while (set.Count() > 1)
+            {
+                var lowers = set.Select(l => l.Item1).ToList();
+                var highers = set.Select(h => h.Item2).ToList();
+                var lowest = lowers.First(l => !highers.Contains(l));
+                pageOrder.Add(lowest);
+                set = rules.Where(r => !pageOrder.Contains(r.Item1)).ToList();
+            }
+            pageOrder.Add(set.First().Item1);
+            pageOrder.Add(set.First().Item2);
+
+            orderCurrent = true;
+        }
+
+        public int[] GetValidOrder(int[] update)
+        {
+            if (!orderCurrent) CalculateOrder();
+            return pageOrder.Where(p=> update.Contains(p)).ToArray();
+        }
+
+        public bool IsValidOrder(int[] update)
+        {
+            var validUpdate = GetValidOrder(update);
+            for (int i = 0; i < update.Length; i++)
+            {
+                if (update[i] != validUpdate[i]) return false;
+            }
+            return true;
+        }
+
+    }
+}
--- Day5/Program.cs
+++ Day5/Program.cs
@@ -6,6 +6,10 @@
         {
             Dictionary<int, PageOrderRule> rules = [];
             List<int[]> updates = [];
+            List<int> lowers = [];
+            List<int> highers = [];
+            PageOrderRules pageOrderRules = new();
+
             using (var sr = new StreamReader("./input/input.txt"))
             {
                 string thisLine;
@@ -20,11 +24,15 @@
                             rules.Add(int.Parse(theRule[0]), new PageOrderRule());
                         }
                         rules[int.Parse(theRule[0])].AddHigher(int.Parse(theRule[1]));
+                        highers.Add(int.Parse(theRule[1]));
                         if (!rules.ContainsKey(int.Parse(theRule[1])))
                         {
                             rules.Add(int.Parse(theRule[1]), new PageOrderRule());
                         }
                         rules[int.Parse(theRule[1])].AddLower(int.Parse(theRule[0]));
+                        lowers.Add(int.Parse(theRule[0]));
+
+                        pageOrderRules.AddRule(int.Parse(theRule[0]), int.Parse(theRule[1]));
                     }
 
                     // update
@@ -35,8 +43,21 @@
                 }
             }
 
+            var validUpdates = updates.Where(u => pageOrderRules.IsValidOrder(u));
+            var validSum = validUpdates.Select(u => u.Skip((u.Length - 1) / 2).Take(1).First()).Sum();
+
+            var invalidUpdates = updates.Where(u => !pageOrderRules.IsValidOrder(u));
+            var fixedUpdates = invalidUpdates.Select(u => pageOrderRules.GetValidOrder(u));
+            var fixedSum = fixedUpdates.Select(u => u.Skip((u.Length - 1) / 2).Take(1).First()).Sum();
+
+            Console.WriteLine($"Sum of the middle numbers of the valid updates is {validSum}");
+            Console.WriteLine($"Sum of the middle numbers of the fixed updates is {fixedSum}");
+
+/*
             // for each update, check if the update is valid
             int middleSum = 0;
+            List<int[]> invalidUpdates = [];
+
             foreach (var update in updates)
             {
                 var valid = true;
@@ -58,9 +79,43 @@
                     if (!valid) break;
                 }
                 if (valid) middleSum += update.Skip((update.Length-1)/2).Take(1).First();
+                else invalidUpdates.Add(update);
             }
 
+            // build the ordered list of pages
+            List<int> orderedPages = [];
+            // get the first page (does not appear in uppers)
+            var firstPage = lowers.Where(l => !highers.Contains(l)).First();
+            orderedPages.Add(firstPage);
+            lowers.RemoveAll(l => l == firstPage);
+            // now loop through lowers and get the rest of the pages
+            while (lowers.Count > 0)
+            {
+                var nextPage = lowers.Where(l => !rules[l].IsInHigherList(orderedPages.Last())).First();
+                orderedPages.Add(nextPage);
+                lowers.RemoveAll(l => l == nextPage);
+            }
+            //while (orderedPages.Count < rules.Count)
+            //{
+            //    var p = rules.Keys.Where(ky=>!orderedPages.Contains(ky)).Select(ky =>
+            //        rules.Where(r => !r.Value.IsInHigherList(ky)).First().Key).First();
+            //    orderedPages.Add(p);
+            //}
+
+            List<int[]> fixedUpdates = [];
+            foreach (var update in invalidUpdates)
+            {
+                // build the proper list of pages for this update
+                var newUpdate = orderedPages.Where(p => update.Contains(p)).ToArray();
+                fixedUpdates.Add(newUpdate);
+            }
+
+            var fixedMiddleSum = fixedUpdates.Select(u => u.Skip((u.Length - 1) / 2).Take(1).First()).Sum();
+
             Console.WriteLine($"Sum of the middle numbers of the valid updates is {middleSum}");
+            Console.WriteLine($"Sum of the middle numbers of the fixed updates is {fixedMiddleSum}");
+
+            */
         }
     }
 }
